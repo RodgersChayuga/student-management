102fdea6cfc33f4676d3c9d284a77fea9c51ebc4
Rodgers Chayuga <rodgers@food4education.org>
2023-11-03 19:31:13 +0300

    feat: wrap paths and components with their specific layouts

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import Home from "./_root/pages/home";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* public routes */}
-        <Route index path="/sign-in" element={<SigninForm />} />
-        <Route index path="/sign-up" element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route index path="/sign-in" element={<SigninForm />} />
+          <Route index path="/sign-up" element={<SignupForm />} />
+        </Route>
 
         {/* private routes */}
-        <Route path="/" element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
