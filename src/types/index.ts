683a2e6b2b7622769fa338b67235f9fc1478ce79
Rodgers Chayuga <rodgers@food4education.org>
2023-11-04 01:34:52 +0300

    feat: implement table with basic functionalities

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface IApplication {
+  id: number;
+  name: string;
+  university: string;
+  country: string;
+  duration: number;
+  cost: number;
+  deadline: string;
+  language: string;
+}
